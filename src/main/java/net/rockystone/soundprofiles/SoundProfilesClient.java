1896ac3402f7faa882383a05991cf76e156bbbfb
Max Brown <maxabro@yahoo.com>
2024-06-05 01:14:55 -0700

    updated to 0.2.0 -- now requires cloth-config

--- src/main/java/net/rockystone/soundprofiles/SoundProfilesClient.java
+++ src/main/java/net/rockystone/soundprofiles/SoundProfilesClient.java
@@ -3,67 +3,48 @@ package net.rockystone.soundprofiles;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.util.InputUtil;
-import net.rockystone.soundprofiles.util.sound.SoundProfile;
-import net.rockystone.soundprofiles.util.sound.SoundProfileSetter;
+import net.minecraft.text.Text;
+import net.rockystone.soundprofiles.util.config.cloth.SoundProfilesConfigScreen;
+import net.rockystone.soundprofiles.util.sound.SoundProfiles;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
 
 @Environment(EnvType.CLIENT)
 public class SoundProfilesClient implements ClientModInitializer {
 
     private static KeyBinding normalVolumeKeyBind;
-    private static KeyBinding quiet1VolumeKeyBind;
+    private static KeyBinding quietVolumeKeyBind;
     private static KeyBinding fullVolumeKeyBind;
     private static KeyBinding customVolumeKeyBind;
+    private static KeyBinding configKeyBind;
 
-    public static final String MOD_ID = "sound-profiles";
+    public static final String MOD_ID = "soundprofiles";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-    public static HashMap<String, SoundProfile> soundProfiles = new HashMap();
 
     @Override
     public void onInitializeClient() {
         LOGGER.info("SoundProfilesClient.onInitializeClient() was called");
 
+        SoundProfiles.createSoundProfiles();
         setKeyBindings();
-        setSoundProfiles();
         registerEvents();
-
     }
 
-    private static void setSoundProfiles() {
-        LOGGER.info("SoundProfilesClient.setSoundProfiles() was called");
 
-        SoundProfile normalSoundProfile = new SoundProfile("sound_profile_normal");
-        SoundProfile quietSoundProfile = new SoundProfile("sound_profile_quiet");
-        SoundProfile fullSoundProfile = new SoundProfile("sound_profile_full");
-        SoundProfile customSoundProfile = new SoundProfile("sound_profile_custom");
-
-        soundProfiles.put("normal", normalSoundProfile);
-        soundProfiles.put("quiet", quietSoundProfile);
-        soundProfiles.put("full", fullSoundProfile);
-        soundProfiles.put("custom", customSoundProfile);
-    }
-
-    public static SoundProfile getSoundProfile(String fileName) {
-        return soundProfiles.getOrDefault(fileName, soundProfiles.get("normal"));
-    }
 
     public void setKeyBindings() {
         LOGGER.info("SoundProfilesClient.setKeyBindings() was called");
 
-        normalVolumeKeyBind = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.soundprofiles.normal_volume", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_KP_7, "category.soundprofiles.keybinds"));
-        quiet1VolumeKeyBind = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.soundprofiles.quiet_volume", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_KP_8, "category.soundprofiles.keybinds"));
-        fullVolumeKeyBind = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.soundprofiles.full_volume", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_KP_9, "category.soundprofiles.keybinds"));
-        customVolumeKeyBind = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.soundprofiles.custom_volume", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_KP_5, "category.soundprofiles.keybinds"));
+        normalVolumeKeyBind = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.soundprofiles.normal_volume", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_KP_7, "category.soundprofiles.keybindings"));
+        quietVolumeKeyBind = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.soundprofiles.quiet_volume", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_KP_8, "category.soundprofiles.keybindings"));
+        fullVolumeKeyBind = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.soundprofiles.full_volume", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_KP_9, "category.soundprofiles.keybindings"));
+        customVolumeKeyBind = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.soundprofiles.custom_volume", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_KP_5, "category.soundprofiles.keybindings"));
+        configKeyBind = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.soundprofiles.config", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_KP_2, "category.soundprofiles.keybindings"));
     }
 
     public void registerEvents() {
@@ -77,19 +58,27 @@ public class SoundProfilesClient implements ClientModInitializer {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
 
             while (normalVolumeKeyBind.wasPressed()) {
-                SoundProfileSetter.setVolumesFromSoundProfile(client, "normal");
+                SoundProfiles.normalSoundProfile.applyProfile(client);
+                client.player.sendMessage(Text.literal("Normal profile loaded"));
             }
 
-            while (quiet1VolumeKeyBind.wasPressed()) {
-                SoundProfileSetter.setVolumesFromSoundProfile(client, "quiet");
+            while (quietVolumeKeyBind.wasPressed()) {
+                SoundProfiles.quietSoundProfile.applyProfile(client);
+                client.player.sendMessage(Text.literal("Quiet profile loaded"));
             }
 
             while (fullVolumeKeyBind.wasPressed()) {
-                SoundProfileSetter.setVolumesFromSoundProfile(client, "full");
+                SoundProfiles.fullSoundProfile.applyProfile(client);
+                client.player.sendMessage(Text.literal("Full profile loaded"));
             }
 
             while (customVolumeKeyBind.wasPressed()) {
-                SoundProfileSetter.setVolumesFromSoundProfile(client, "custom");
+                SoundProfiles.customSoundProfile.applyProfile(client);
+                client.player.sendMessage(Text.literal("Custom profile loaded"));
+            }
+
+            while (configKeyBind.wasPressed()) {
+                SoundProfilesConfigScreen.start();
             }
 
         });
--- src/main/java/net/rockystone/soundprofiles/util/sound/SoundProfile.java
+++ src/main/java/net/rockystone/soundprofiles/util/sound/SoundProfile.java
@@ -1,83 +1,188 @@
 package net.rockystone.soundprofiles.util.sound;
 
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.sound.SoundCategory;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class SoundProfile {
-    private Map<String, Double> volumes;
     private Path path;
+    public String name;
+    private List<Double> volumes;
 
-    private static final String soundProfilesDirectory = "sound-profiles_profiles";
+    private static final String soundProfilesDirectory = "soundprofiles_profiles";
     private static final String configDir = FabricLoader.getInstance().getConfigDir().toString();
 
-    public SoundProfile(String fileName) {
-        path = Paths.get(configDir,soundProfilesDirectory, fileName);
-        genSoundProfilesDirectory();
-        genDefaultSoundProfile();
+    public SoundProfile(String name) {
+        this.name = name;
+        path = Paths.get(configDir, soundProfilesDirectory, name);
+
+        genSoundProfilesDirectoryIfAbsent();
         volumes = readFile(path);
     }
 
-    private HashMap<String, Double> readFile(Path path) {
-        HashMap<String, Double> returnVolumes = new HashMap();
+    private ArrayList<Double> readFile(Path path) {
+        ArrayList<Double> returnVolumes = new ArrayList();
 
         try {
             File file = path.toFile();
-            Scanner reader = new Scanner(file);
+            if (!file.exists()) {
+                writeFile();
+            }
 
+            Scanner reader = new Scanner(file);
             while (reader.hasNextLine()) {
                 String[] parts = reader.nextLine().split(",");
                 if (parts[0].equals("SOUND_CATEGORY")) {
                     continue;
                 }
-                returnVolumes.put(parts[0], Double.valueOf(parts[1]));
+                returnVolumes.add(Double.valueOf(parts[1]));
             }
-        } catch (Exception e) {
-
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         return returnVolumes;
     }
 
-    public Map<String, Double> getVolumes() {
-        return volumes;
-    }
-
-    public void refresh() {
-        volumes = readFile(path);
-    }
-
-    private void genSoundProfilesDirectory() {
-        new File(configDir, soundProfilesDirectory).mkdirs();
-    }
-
-    private void genDefaultSoundProfile() {
+    private void writeFile() {
         try {
 
             File file = path.toFile();
-            if (!file.exists()) {
-                FileWriter writer = new FileWriter(file);
-                writer.write("SOUND_CATEGORY,DOUBLE_FROM_0.0_TO_1.0\n");
-                writer.write("MASTER,0.5\n");
-                writer.write("HOSTILE,0.5\n");
-                writer.write("AMBIENT,0.5\n");
-                writer.write("BLOCKS,0.5\n");
-                writer.write("MUSIC,0.5\n");
-                writer.write("NEUTRAL,0.5\n");
-                writer.write("PLAYERS,0.5\n");
-                writer.write("RECORDS,0.5\n");
-                writer.write("VOICE,0.5\n");
-                writer.write("WEATHER,0.5");
-                writer.close();
+            if (!file.exists() || volumes.isEmpty()) {
+                setDefaultVolumes();
             }
-        } catch (Exception e) {
+            FileWriter writer = new FileWriter(file);
+            writer.write("SOUND_CATEGORY,DOUBLE_FROM_0.0_TO_1.0\n");
+            writer.write("MASTER," + volumes.get(0) + "\n");
+            writer.write("HOSTILE," + volumes.get(1) + "\n");
+            writer.write("AMBIENT," + volumes.get(2) + "\n");
+            writer.write("BLOCKS," + volumes.get(3) + "\n");
+            writer.write("MUSIC," + volumes.get(4) + "\n");
+            writer.write("NEUTRAL," + volumes.get(5) + "\n");
+            writer.write("PLAYERS," + volumes.get(6) + "\n");
+            writer.write("RECORDS," + volumes.get(7) + "\n");
+            writer.write("VOICE," + volumes.get(8) + "\n");
+            writer.write("WEATHER," + volumes.get(9) + "\n");
+            writer.close();
 
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    public List<Double> getVolumes() {
+        return volumes;
+    }
+
+    public void setVolumeValues(List<Double> volumes) {
+        this.volumes = volumes;
+        writeFile();
+    }
+
+    public void applyProfile(MinecraftClient client) {
+        client.options.getSoundVolumeOption(SoundCategory.MASTER).setValue(volumes.get(0));
+        client.options.getSoundVolumeOption(SoundCategory.HOSTILE).setValue(volumes.get(1));
+        client.options.getSoundVolumeOption(SoundCategory.AMBIENT).setValue(volumes.get(2));
+        client.options.getSoundVolumeOption(SoundCategory.BLOCKS).setValue(volumes.get(3));
+        client.options.getSoundVolumeOption(SoundCategory.MUSIC).setValue(volumes.get(4));
+        client.options.getSoundVolumeOption(SoundCategory.NEUTRAL).setValue(volumes.get(5));
+        client.options.getSoundVolumeOption(SoundCategory.PLAYERS).setValue(volumes.get(6));
+        client.options.getSoundVolumeOption(SoundCategory.RECORDS).setValue(volumes.get(7));
+        client.options.getSoundVolumeOption(SoundCategory.VOICE).setValue(volumes.get(8));
+        client.options.getSoundVolumeOption(SoundCategory.WEATHER).setValue(volumes.get(9));
+    }
+
+    //Below methods used for setting/generating default values for first run of modded client
+    private void setDefaultVolumes() {
+        double master, hostile, ambient, blocks, music, neutral, players, records, voice, weather;
+
+        switch (name) {
+            case "normal":
+                master = 0.75;
+                hostile = 1.0;
+                ambient = 1.0;
+                blocks = 0.75;
+                music = 0.3;
+                neutral = 0.2;
+                players = 1.0;
+                records = 0.25;
+                voice = 1.0;
+                weather = 0.2;
+                break;
+
+            case "quiet":
+                master = 0.1;
+                hostile = 0.1;
+                ambient = 0.1;
+                blocks = 0.1;
+                music = 0.1;
+                neutral = 0.1;
+                players = 0.1;
+                records = 0.1;
+                voice = 0.1;
+                weather = 0.1;
+                break;
+
+            case "full":
+                master = 1.0;
+                hostile = 1.0;
+                ambient = 1.0;
+                blocks = 1.0;
+                music = 1.0;
+                neutral = 1.0;
+                players = 1.0;
+                records = 1.0;
+                voice = 1.0;
+                weather = 1.0;
+                break;
+
+            case "custom":
+                master = 0.5;
+                hostile = 0.5;
+                ambient = 0.5;
+                blocks = 0.5;
+                music = 0.5;
+                neutral = 0.5;
+                players = 0.5;
+                records = 0.5;
+                voice = 0.5;
+                weather = 0.5;
+                break;
+
+            default:
+                master = 1.0;
+                hostile = 1.0;
+                ambient = 1.0;
+                blocks = 1.0;
+                music = 1.0;
+                neutral = 1.0;
+                players = 1.0;
+                records = 1.0;
+                voice = 1.0;
+                weather = 0.5;
+        }
+        volumes = new ArrayList();
+        volumes.add(master);
+        volumes.add(hostile);
+        volumes.add(ambient);
+        volumes.add(blocks);
+        volumes.add(music);
+        volumes.add(neutral);
+        volumes.add(players);
+        volumes.add(records);
+        volumes.add(voice);
+        volumes.add(weather);
+    }
+
+    private void genSoundProfilesDirectoryIfAbsent() {
+        new File(configDir, soundProfilesDirectory).mkdirs();
     }
 }
